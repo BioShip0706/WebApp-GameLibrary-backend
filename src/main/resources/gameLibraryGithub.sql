79a69426e8f656385c30153673fc43d395ff4c3e
ricca <riccardosarritzu25@gmail.com>
2025-06-23 19:14:39 +0200

    Added the sql file to the repo

---
+++ src/main/resources/gameLibraryGithub.sql
@@ -0,0 +1,167 @@
+
+-- Insert giochi
+INSERT INTO game (description, developer, imageurl, publisher, release_date, score, title) VALUES
+("An open world RPG about a monster hunter.","CD Projekt Red","https://img.freepik.com/premium-vector/stick-controller-art_262962-218.jpg","CD Projekt","2015-05-19",9.7,"The Witcher 3: Wild Hunt"),
+("Dark fantasy open world game.","FromSoftware","https://static0.gamerantimages.com/wordpress/wp-content/uploads/2022/11/elden-ring-cover.jpg","Bandai Namco","2022-02-25",9.4,"Elden ring"),
+("Western-themed open world game.","Rockstar Games","https://img.freepik.com/premium-vector/stick-controller-art_262962-218.jpg","Rockstar Games","2018-10-26",9.8,"Red Dead Redemption 2"),
+("Sci-fi RPG set in a dystopian future.","CD Projekt Red","https://img.freepik.com/premium-vector/stick-controller-art_262962-218.jpg","CD Projekt","2020-12-10",8.5,"Cyberpunk 2077"),
+("Action-adventure with Norse mythology.","Santa Monica Studio","https://img.freepik.com/premium-vector/stick-controller-art_262962-218.jpg","Sony","2022-11-09",9.5,"God of War Ragnarok"),
+("Sci-fi open world adventure.","Guerrilla Games","https://img.freepik.com/premium-vector/stick-controller-art_262962-218.jpg","Sony","2022-02-18",8.9,"Horizon Forbidden West"),
+("Sandbox building game.","Mojang","https://img.freepik.com/premium-vector/stick-controller-art_262962-218.jpg","Mojang","2011-11-18",9,"Minecraft"),
+("Farming and life simulator.","ConcernedApe","https://img.freepik.com/premium-vector/stick-controller-art_262962-218.jpg","ConcernedApe","2016-02-26",9.2,"Stardew Valley"),
+("Open world action-adventure.","Nintendo","https://img.freepik.com/premium-vector/stick-controller-art_262962-218.jpg","Nintendo","2017-03-03",9.8,"The Legend of Zelda: Breath of the Wild"),
+("3D platformer with Mario.","Nintendo","https://img.freepik.com/premium-vector/stick-controller-art_262962-218.jpg","Nintendo","2017-10-27",9.5,"Super Mario Odyssey"),
+("Challenging action RPG.","FromSoftware","https://img.freepik.com/premium-vector/stick-controller-art_262962-218.jpg","Bandai Namco","2016-04-12",9.3,"Dark Souls III"),
+("Gothic horror action RPG.","FromSoftware","https://img.freepik.com/premium-vector/stick-controller-art_262962-218.jpg","Sony","2015-03-24",9.4,"Bloodborne"),
+("Action game with stealth elements.","FromSoftware","https://img.freepik.com/premium-vector/stick-controller-art_262962-218.jpg","Activision","2019-03-22",9.2,"Sekiro: Shadows Die Twice"),
+("Roguelike dungeon crawler.","Supergiant Games","https://img.freepik.com/premium-vector/stick-controller-art_262962-218.jpg","Supergiant Games","2020-09-17",9,"Hades"),
+("Indie platformer about climbing a mountain.","Matt Makes Games","https://img.freepik.com/premium-vector/stick-controller-art_262962-218.jpg","Matt Makes Games","2018-01-25",9.1,"Celeste"),
+("Horror FPS.","Capcom","https://img.freepik.com/premium-vector/stick-controller-art_262962-218.jpg","Capcom","2021-05-07",8.7,"Resident Evil Village"),
+("Fast-paced first person shooter.","id Software","https://img.freepik.com/premium-vector/stick-controller-art_262962-218.jpg","Bethesda","2020-03-20",9,"Doom Eternal"),
+("Social deduction game.","Innersloth","https://img.freepik.com/premium-vector/stick-controller-art_262962-218.jpg","Innersloth","2018-06-15",8.1,"Among Us"),
+("Multiplayer party platformer.","Mediatonic","https://img.freepik.com/premium-vector/stick-controller-art_262962-218.jpg","Epic Games","2020-08-04",8,"Fall Guys"),
+("Open world anime RPG.","miHoYo","https://img.freepik.com/premium-vector/stick-controller-art_262962-218.jpg","miHoYo","2020-09-28",8.6,"Genshin Impact"),
+("MOBA with huge player base.","Riot Games","https://img.freepik.com/premium-vector/stick-controller-art_262962-218.jpg","Riot Games","2009-10-27",9.1,"League of Legends"),
+("Tactical FPS with abilities.","Riot Games","https://img.freepik.com/premium-vector/stick-controller-art_262962-218.jpg","Riot Games","2020-06-02",8.8,"Valorant"),
+("Team-based FPS.","Blizzard","https://img.freepik.com/premium-vector/stick-controller-art_262962-218.jpg","Blizzard","2022-10-04",7.9,"Overwatch 2"),
+("Life simulation game.","Maxis","https://img.freepik.com/premium-vector/stick-controller-art_262962-218.jpg","Electronic Arts","2014-09-02",8.2,"The Sims 4"),
+("Football simulation game.","EA Sports","https://img.freepik.com/premium-vector/stick-controller-art_262962-218.jpg","Electronic Arts","2022-09-30",8,"FIFA 23"),
+("Basketball simulation game.","Visual Concepts","https://img.freepik.com/premium-vector/stick-controller-art_262962-218.jpg","2K Sports","2023-09-08",7.8,"NBA 2K24"),
+("Open world action RPG set in Viking era.","Ubisoft Montreal","https://img.freepik.com/premium-vector/stick-controller-art_262962-218.jpg","Ubisoft","2020-11-10",8.5,"Assassin's Creed Valhalla"),
+("FPS set in a tropical island.","Ubisoft Toronto","https://img.freepik.com/premium-vector/stick-controller-art_262962-218.jpg","Ubisoft","2021-10-07",7.9,"Far Cry 6"),
+("Stylish JRPG.","Atlus","https://img.freepik.com/premium-vector/stick-controller-art_262962-218.jpg","Sega","2020-03-31",9.6,"Persona 5 Royal"),
+("Isometric Zelda-like adventure.","Andrew Shouldice","https://img.freepik.com/premium-vector/stick-controller-art_262962-218.jpg","Finji","2022-03-16",8.5,"Tunic");
+
+
+-- Insert piattaforme elenco
+INSERT INTO platform (name) VALUES
+('Android'),
+('Browser'),
+('Google Stadia'),
+('iOS'),
+('Linux'),
+('MacOS'),
+('Nintendo 3DS'),
+('Nintendo DS'),
+('Nintendo Switch'),
+('Nintendo Wii U'),
+('PC'),
+('PlayStation 3'),
+('PlayStation 4'),
+('PlayStation 5'),
+('PlayStation Vita'),
+('PSP'),
+('Steam Deck'),
+('Xbox 360'),
+('Xbox One'),
+('Xbox Series X');
+
+-- Insert generi elenco
+INSERT INTO genre (name) VALUES
+('Action'),
+('Adventure'),
+('Fighting'),
+('Horror'),
+('Metroidvania'),
+('MMORPG'),
+('Party'),
+('Platformer'),
+('Puzzle'),
+('Racing'),
+('Rhythm'),
+('Role-playing'),
+('Sandbox'),
+('Shooter'),
+('Simulation'),
+('Sports'),
+('Stealth'),
+('Strategy'),
+('Survival'),
+('Visual Novel');
+
+
+
+-- Insert generi
+INSERT INTO game_genres (game_id, genres_id) VALUES
+(1,1),(1,12),
+(2,1),(2,12),
+(3,1),(3,12),
+(4,1),(4,12),
+(5,1),(5,12),
+(6,1),(6,12),
+(7,13),(7,15),
+(8,15),(8,12),
+(9,2),(9,8),
+(10,8),
+(11,1),(11,12),
+(12,1),(12,12),
+(13,1),(13,17),
+(14,1),(14,12),
+(15,8),
+(16,4),(16,1),
+(17,14),(17,1),
+(18,7),(18,18),
+(19,7),
+(20,12),(20,2),
+(21,6),(21,18),
+(22,14),
+(23,14),(23,1),
+(24,15),
+(25,16),
+(26,16),
+(27,1),(27,2),(27,12),
+(28,14),(28,1),(28,2),
+(29,12),(29,2),
+(30,2),(30,12);
+
+
+-- game_platforms
+INSERT INTO game_platforms (game_id, platforms_id) VALUES
+(1,11),(1,13),(1,19),(1,9),
+(2,11),(2,13),(2,14),(2,19),(2,20),(2,17),
+(3,11),(3,13),(3,19),(3,3),
+(4,11),(4,13),(4,14),(4,19),(4,20),(4,3),
+(5,11),(5,13),(5,14),
+(6,11),(6,13),(6,14),
+(7,11),(7,6),(7,5),(7,1),(7,4),(7,13),(7,14),(7,9),
+(8,11),(8,6),(8,5),(8,13),(8,14),(8,19),(8,20),(8,9),(8,1),(8,4),(8,15),
+(9,9),(9,10),
+(10,9),
+(11,11),(11,13),(11,19),(11,9),
+(12,13),
+(13,11),(13,13),(13,19),
+(14,11),(14,6),(14,5),(14,9),(14,13),(14,14),(14,19),(14,20),(14,17),
+(15,11),(15,6),(15,5),(15,13),(15,19),(15,9),
+(16,11),(16,13),(16,14),(16,19),(16,20),
+(17,11),(17,13),(17,14),(17,19),(17,20),(17,9),
+(18,11),(18,1),(18,4),(18,9),(18,13),(18,14),(18,19),(18,20),
+(19,11),(19,13),(19,14),(19,19),(19,20),(19,9),
+(20,11),(20,1),(20,4),(20,13),(20,14),
+(21,11),
+(22,11),
+(23,11),(23,13),(23,14),(23,19),(23,20),(23,9),
+(24,11),(24,6),(24,13),(24,14),(24,19),(24,20),
+(25,11),(25,13),(25,14),(25,19),(25,20),
+(26,11),(26,13),(26,14),(26,19),(26,20),
+(27,11),(27,13),(27,14),(27,19),(27,20),
+(28,11),(28,13),(28,14),(28,19),(28,20),(28,3),
+(29,13),(29,9),(29,11),
+(30,11),(30,18),(30,20);
+
+
+
+
+
+-- Insert utenti
+INSERT INTO user (created_at, updated_at, enabled, password, username) VALUES
+('2025-06-23 11:37:06.218230', '2025-06-23 11:37:06.218230', 0, '$2a$10$a3spu6oLeFgQy74ZH8LlJ.9UPCpYZYH4wkVOjE365V4MOngvO85oS', 'AdminUser');
+
+-- Insert ruoli
+INSERT INTO authority (authority_name, default_authority, visible) VALUES
+('ROLE_ADMIN', 0, 1),
+('ROLE_MEMBER', 1, 1);
+
+-- Insert ruolo utente
+INSERT INTO user_authorities (user_id, authority_id) VALUES
+(1, 1),
+(1, 2);
